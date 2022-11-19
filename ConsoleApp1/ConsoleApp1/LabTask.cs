b26d61c4bfdedbf52c01cb6a4d9a17a5d9ec48f8
Rovshan Guliyev <rovshanshg@code.edu.az>
2022-11-19 16:52:37 +0400

    Lab18.11.2022

---
+++ ConsoleApp1/ConsoleApp1/LabTask.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class LabTask
+    {
+        public void ElementOfArray(string[] array)
+        {
+
+            //foreach (var item in array)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+
+
+
+
+        }
+
+        public void DioganalDifference(int[,] arr)
+        {
+            int lefttoright = 0;
+            int rigthtoleft = 0;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                lefttoright = lefttoright + arr[i, i];
+            }
+
+
+
+            int j = arr.GetLength(0) - 1;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                rigthtoleft += arr[i, j];
+                j--;
+            }
+
+            Console.WriteLine(lefttoright - rigthtoleft);
+
+        }
+
+
+
+        public string Trim(string str)
+        {
+            string result = "";
+            int start = 0;
+            int end = str.Length - 1;
+
+            while (str[start] == ' ')
+            {
+                start++;
+            }
+
+            while (str[end] == ' ')
+            {
+                end--;
+            }
+
+            result = str.Substring(start, end - start + 1);
+
+            return result;
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LabTask labTask = new LabTask();
+
+            //string[] students = { "Ayaz", "Rasim", "Aytac" };
+
+            //labTask.ElementOfArray(students);
+
+            //int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
+
+            //labTask.DioganalDifference(arr);
+
+            Console.WriteLine(labTask.Trim("   P515"));
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
